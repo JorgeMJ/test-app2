02f632241661e20be52445eaa6bd57c04b4ef7cb
Jorge <jorge.joven1@gmail.com>
2022-03-29 19:02:38 -0400

    add initial page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'say/index'
+  root to: 'say#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
